7f7663976629856848fad0c801a5f1ddba544166
Dmitry Zharikov <dimahzharikov10@gmail.com>
2023-04-23 22:13:34 +0300

    [ADD] New models and hypothesis

--- utils_functionality/models/blending.py
+++ utils_functionality/models/blending.py
@@ -4,43 +4,38 @@ from catboost import CatBoostClassifier
 from numpy import hstack
 
 
-def get_models():
-    models = list()
-    models.append(('lgbm', LGBMClassifier()))
-    models.append(('xgb', XGBClassifier()))
-    return models
+class BlendingModels:
+    def __init__(self):
+        self.lgbm = LGBMClassifier()
+        self.xgb = XGBClassifier()
+        self.models = [("lgbm", self.lgbm), ("xgb", self.xgb)]
 
+    def fit(self, X_train, X_val, y_train, y_val):
+        # fit all models on the training set and predict on hold out set
+        meta_X = list()
+        for _, model in self.models:
+            # fit in training set
+            model.fit(X_train, y_train)
+            # predict on hold out set
+            yhat = model.predict(X_val)
+            yhat = yhat.reshape(len(yhat), 1)
+            # store predictions as input for blending
+            meta_X.append(yhat)
+        # create 2d array from predictions, each set is an input feature
+        meta_X = hstack(meta_X)
+        # define blending model
+        self.blender = CatBoostClassifier(verbose=False)
+        # fit on predictions from base models
+        self.blender.fit(meta_X, y_val)
+        return self.blender
 
-def fit_ensemble(models, X_train, X_val, y_train, y_val):
-    # fit all models on the training set and predict on hold out set
-    meta_X = list()
-    for _, model in models:
-        # fit in training set
-        model.fit(X_train, y_train)
-        # predict on hold out set
-        yhat = model.predict_proba(X_val)
-        # store predictions as input for blending
-        meta_X.append(yhat)
-    # create 2d array from predictions, each set is an input feature
-    meta_X = hstack(meta_X)
-    # define blending model
-    blender = CatBoostClassifier(verbose=False)
-    # fit on predictions from base models
-    blender.fit(meta_X, y_val)
-    return blender
-
-# make a prediction with the blending ensemble
-
-
-def predict_ensemble(models, blender, X_test):
-    # make predictions with base models
-    meta_X = list()
-    for _, model in models:
-        # predict with base model
-        yhat = model.predict_proba(X_test)
-        # store prediction
-        meta_X.append(yhat)
-    # create 2d array from predictions, each set is an input feature
-    meta_X = hstack(meta_X)
-    # predict
-    return blender.predict(meta_X)
+    def predict(self, X_test):
+        meta_X = list()
+        for _, model in self.models:
+            yhat = model.predict(X_test)
+            yhat = yhat.reshape(len(yhat), 1)
+            # store predictions as input for blending
+            meta_X.append(yhat)
+        # create 2d array from predictions, each set is an input feature
+        meta_X = hstack(meta_X)
+        return self.blender.predict(X_test)
--- utils_functionality/models/class_models.py
+++ utils_functionality/models/class_models.py
@@ -3,49 +3,75 @@ import pandas as pd
 
 
 class FeatureWizModel:
-    def __init__(self, df, target, target_columns, verbose):
+    def __init__(self, df, target, verbose):
         self.target = target
         self.df = df.copy()
         self.verbose = verbose
 
     def feature_selection(self, corr_limit=0.7):
         self.feats, self.df_featurewiz = featurewiz(
-            self.df,
-            self.target,
-            corr_limit=0.7,
-            verbose=self.verbose
+            self.df, self.target, corr_limit=corr_limit, verbose=self.verbose
         )
 
 
 class CreateSamples:
-    def __init__(self, df, train, test, target, features=None, dummies=False, use_featurewiz=False, corr_limit=0.7, verbose=0):
-        self.target_columns = ['splashing',
-                               'breaking_up', 'net_impact', 'rebound', 'one_drop']
+    def __init__(
+        self,
+        df,
+        train,
+        test,
+        target,
+        features=None,
+        dummies=False,
+        use_featurewiz=False,
+        corr_limit=0.7,
+        strange_columns=None,
+        drop_duplicates=False,
+        verbose=0,
+    ):
+        self.train, self.test = train.copy(), test.copy()
+        self.target_columns = [
+            "splashing",
+            "breaking_up",
+            "net_impact",
+            "rebound",
+            "one_drop",
+        ]
         self.target = target
         self.features = features
-        drop_columns = list(set(self.target_columns) -
-                            set([target])) + ['test']
-        obj_columns = df.select_dtypes(include=['object']).columns
+        drop_columns = list(set(self.target_columns) - set([target])) + ["test"]
+        if strange_columns is not None:
+            drop_columns += strange_columns
+        obj_columns = df.select_dtypes(include=["object"]).columns
         if dummies:
-            self.df = pd.get_dummies(df, columns=obj_columns)[
-                features+[target]]
+            self.df = pd.get_dummies(df, columns=obj_columns)[features + [target]]
         else:
             self.df = df.drop(columns=drop_columns, axis=1).copy()
         if use_featurewiz:
-            fwiz = FeatureWizModel(
-                df=self.df, verbose=verbose, target=target, target_columns=self.target_columns,
-            )
+            fwiz = FeatureWizModel(df=self.df, verbose=verbose, target=target)
             fwiz.feature_selection(corr_limit)
             self.df, self.features = fwiz.df_featurewiz, fwiz.feats
-
         self.y = self.df[self.target]
         if self.features is not None:
             to_drop = set(self.df.columns) - set(self.features)
             self.X = self.df.drop(to_drop, axis=1)
         else:
             self.X = self.df.drop(target, axis=1)
-        self.X_train, self.y_train = self.X.loc[train.index], self.y[train.index]
-        self.X_test, self.y_test = self.X.loc[test.index], self.y[test.index]
+        self.X_train, self.y_train = (
+            self.X.loc[self.train.index],
+            self.y.loc[self.train.index],
+        )
+        self.X_test, self.y_test = (
+            self.X.loc[self.test.index],
+            self.y.loc[self.test.index],
+        )
+        if drop_duplicates:
+            self.X_train = self.X_train.drop_duplicates()
+            self.X_test = self.X_test.drop_duplicates()
+            self.y_train = self.y_train.loc[self.X_train.index]
+            self.y_test = self.y_test.loc[self.X_test.index]
+            print(f"Shape train:\t {self.X_train.shape}")
+            print(f"Shape test:\t {self.X_test.shape}")
 
     def get_samples(self):
         return self.X_train, self.X_test, self.y_train, self.y_test
